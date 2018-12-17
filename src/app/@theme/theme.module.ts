8d70a9624139eb9ab9fc6290364f75aff71f46d7
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2018-12-17 17:47:21 -0500

    removed ng-bootstrap as it in unnecessary

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { DirectivesModule } from "@shared/directives/directives.module";
 
 import { FooterComponent, HeaderComponent } from "./components";
@@ -12,47 +11,46 @@ import { CategoriesComponent, SearchComponent, TagsComponent } from "./widgets";
 
 
 const BASE_MODULES = [
-    CommonModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
+	CommonModule,
+	RouterModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	ReactiveFormsModule,
 ];
 
 const MODULES = [
-    NgbModule,
-    DirectivesModule,
+	DirectivesModule,
 ];
 
 const COMPONENTS = [
-    //  components
-    FooterComponent,
-    HeaderComponent,
+	//  components
+	FooterComponent,
+	HeaderComponent,
 
-    //  Layouts
-    AppComponent,
-    HomeComponent,
-    BlogComponent,
+	//  Layouts
+	AppComponent,
+	HomeComponent,
+	BlogComponent,
 
-    //  widgets
-    CategoriesComponent,
-    SearchComponent,
-    TagsComponent,
+	//  widgets
+	CategoriesComponent,
+	SearchComponent,
+	TagsComponent,
 ];
 
 const PROVIDERS = [];
 
 @NgModule({
-    imports      : [ ...BASE_MODULES, ...MODULES ],
-    declarations : [ ...COMPONENTS ],
-    exports      : [ ...COMPONENTS ],
+	imports     : [ ...BASE_MODULES, ...MODULES ],
+	declarations: [ ...COMPONENTS ],
+	exports     : [ ...COMPONENTS ],
 })
 export class ThemeModule {
-    static forRoot(): ModuleWithProviders {
-        return <ModuleWithProviders>{
-            ngModule  : ThemeModule,
-            providers : [ ...PROVIDERS ],
-        };
-    }
+	static forRoot(): ModuleWithProviders {
+		return <ModuleWithProviders>{
+			ngModule : ThemeModule,
+			providers: [ ...PROVIDERS ],
+		};
+	}
 }
 
